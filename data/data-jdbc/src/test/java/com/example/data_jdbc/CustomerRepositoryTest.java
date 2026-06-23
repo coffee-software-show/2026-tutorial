bca9a9b160283f3c04eece2acd8bb917ea124845
Josh Long <joshua.long@broadcom.com>
2026-06-23 17:05:48 +0900

    up

--- data/data-jdbc/src/test/java/com/example/data_jdbc/CustomerRepositoryTest.java
+++ data/data-jdbc/src/test/java/com/example/data_jdbc/CustomerRepositoryTest.java
@@ -6,10 +6,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jdbc.test.autoconfigure.DataJdbcTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.test.annotation.Commit;
 
+import java.util.List;
 import java.util.Set;
 
+
+@Configuration
+class DataTestConfiguration {
+
+    @Bean
+    SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
+    }
+}
+
+@Import(DataTestConfiguration.class)
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest {
@@ -26,13 +46,16 @@ class CustomerRepositoryTest {
 
     @BeforeEach
     void before() {
+        //
         this.repository.deleteAll();
         this.customer = this.repository.save(new Customer(null, this.username,
                 "Josh Long", Set.of(
                 new LineItem(null, 1, "sku1"),
                 new LineItem(null, 2, "sku2"))));
-    }
 
+        //
+
+    }
 
     @Commit
     @Test
@@ -45,6 +68,27 @@ class CustomerRepositoryTest {
                 .stream().anyMatch(li -> li.quantity() == 1 && li.sku().equals("sku1")));
         Assertions.assertTrue(byEmail.lineItems()
                 .stream().anyMatch(li -> li.quantity() == 2 && li.sku().equals("sku2")));
+
+    }
+
+    @Test
+    @Commit
+    void secure() {
+
+        var user = new User(this.username, "", true, true, true, true,
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        var authentication = new UsernamePasswordAuthenticationToken(user, null);
+
+        SecurityContextHolder
+                .getContextHolderStrategy()
+                .getContext()
+                .setAuthentication(authentication);
+
+        var customer = this.repository.findByAuthenticatedUser();
+        IO.println("Authenticated user: " + customer.username());
+        Assertions.assertNotNull(customer, "the customer " + this.username
+                + " must not be null");
 
     }
 }
